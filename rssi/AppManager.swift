21801c4d8107f1ec2423a51a45e0dd145eea0c09
Dan Morton <dmorton2297@gmail.com>
2017-08-10 10:21:58 -0500

    Can delete gateways off map

--- rssi/AppManager.swift
+++ rssi/AppManager.swift
@@ -89,6 +89,36 @@ class AppManager {
         return maps
     }
     
+    func deleteMapFromHospital(hospitalName: String, mapName: String) {
+        let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let hospitalFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Hospital_")
+        do {
+            let fetchedHospitals = try managedObjectContext.fetch(hospitalFetch)
+            
+            for i in 0..<fetchedHospitals.count {
+                let entry = fetchedHospitals[i] as! Hospital_
+                //print((entry as AnyObject).value(forKey: "name") ?? "Could not fetch name values in AppManager::loadDataFromDataStore")
+                let name = (entry as AnyObject).value(forKey: "name") as! String
+                if name == hospitalName {
+                    for map in entry.relationship! {
+                        let m = map as! Plot_
+                        entry.removeFromRelationship(m)
+                    }
+                }
+            }
+            
+        } catch {
+            fatalError("Could not load hospital data in AddMapViewController")
+        }
+        
+        do {
+            try managedObjectContext.save()
+        } catch {
+            fatalError("Could not save data change to core data")
+        }
+
+    }
+    
     func saveGatewayToMap(hospitalName: String, mapName: String, gateway: Gateway) {
         let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let hospitalFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Hospital_")
